25639e2b31e4ad6e91c245b95bab2320f6404cec
ict-murai <105769035+ict-murai@users.noreply.github.com>
2022-07-12 15:57:03 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,9 @@
 <?php 
   foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    $pos = strpos($line);
-    echo $pos;
+    $pos = strpos($line ,'abc');
+    if ($pos !== false) {
+     echo　＄line
+    }
   } 
 ?>
